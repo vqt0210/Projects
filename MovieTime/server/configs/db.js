c6153efe6c42094f5ac483d54f796e96acf6acd1
vqt-zz <tuanvq@larion.com>
2025-09-17 16:40:02 +0700

    update email field

--- MovieTime/server/configs/db.js
+++ MovieTime/server/configs/db.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () =>{
   try {
+    // Khi kết nối thành công thì mongoose phát ra event 'connected'
     mongoose.connection.on('connected', ()=> console.log('Database connected'));
+
+    // Thực hiện kết nối tới MongoDB
     await mongoose.connect(`${process.env.MONGODB_URI}/movietime`)
   } catch (error) {
+    // Nếu có lỗi khi kết nối thì in ra
     console.log(error.message);
   }
 }
--- MovieTime/server/inngest/index.js
+++ MovieTime/server/inngest/index.js
@@ -12,7 +12,7 @@ const syncUserCreation = inngest.createFunction(
     const {id, first_name, last_name, email_addresses, image_url} = event.data
     const userData = {
       _id: id,
-      email: email_addresses[0].email_addresses,
+      email: email_addresses[0].email_address,
       name: first_name + ' ' + last_name,
       image: image_url
     }
