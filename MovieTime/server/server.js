fb61e6ba20f3364dbe2e3f7397aad043ee3500a2
vqt-zz <tuanvq@larion.com>
2025-09-22 17:24:51 +0700

    feat(api): add show, booking, admin, and user routes with corresponding routers

--- MovieTime/server/server.js
+++ MovieTime/server/server.js
@@ -5,6 +5,10 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -21,5 +25,9 @@ app.use(clerkMiddleware())
 // API Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, ()=> console.log(`Server is listening at http://localhost:${port}`))
