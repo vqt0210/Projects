1980b4a420ac44d95889aeb9e1d235da417afaf1
vqt-zz <tuanvq@larion.com>
2025-09-22 17:13:54 +0700

    Create a Show model

---
+++ MovieTime/server/models/Show.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const showSchema = new mongoose.Schema(
+  {
+    movie: {type: String, required: true, ref: 'Movie'},
+    showDateTime: {type: Date, required: true},
+    showPrice: {type: Object, default: {}}
+  }, {minimize: false}
+)
+
+const Show = mongoose.model("Show" ,showSchema);
+
+export default Show;
