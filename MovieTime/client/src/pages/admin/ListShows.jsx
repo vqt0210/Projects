d7532f18bce29eed5317fcbfb1ca5b816a17c80a
vqt-zz <tuanvq@larion.com>
2025-09-23 17:39:48 +0700

    feat(shows): add admin API to list all shows and integrate ListShows page

--- MovieTime/client/src/pages/admin/ListShows.jsx
+++ MovieTime/client/src/pages/admin/ListShows.jsx
@@ -3,25 +3,22 @@ import { dummyShowsData } from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import { dateFormat } from "../../lib/dateFormat";
+import { useAppContext } from "../../context/AppContext";
 
 const ListShows = () => {
 
   const currency = import.meta.env.VITE_CURRENCY
+
+  const {axios, getToken, user} = useAppContext()
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getAllShows = async () => {
     try {
-      setShows([{
-        movie: dummyShowsData[0],
-        showDateTime: "2025-06-30T02:30:00.000Z",
-        showPrice: 59,
-        occupiedSeats: {
-          A1: "user_1",
-          B1: "user_2",
-          C1: "user_3"
-        }
-      }]);
+      const {data} = await axios.get('/api/admin/all-shows', {
+        headers: {Authorization: `Bearer ${await getToken()}`}
+      });
+      setShows(data.shows)
       setLoading(false);
     } catch (error){
         console.error(error);
@@ -29,8 +26,11 @@ const ListShows = () => {
   }
 
   useEffect(() => {
-    getAllShows();
-},[]);
+    if(user){
+        getAllShows();
+    }
+    
+},[user]);
   return !loading ? (
     <>
       <Title text1="List" text2="Shows" />
@@ -50,8 +50,10 @@ const ListShows = () => {
               <tr key={index} className="border-b border-primary/10 bg-primary/5 even:bg-primary/10">
                 <td className="p-2 min-w-45 pl-5">{show.movie.title}</td>
                 <td className="p-2">{dateFormat(show.showDateTime)}</td>
-                <td className="p-2">{Object.keys(show.occupiedSeats).length}</td>
-                <td className="p-2">{currency} {Object.keys(show.occupiedSeats).length * show.showPrice}</td>
+                <td className="p-2">{Object.keys(show.occupiedSeats || {}).length}</td>
+                <td className="p-2">
+                    {currency} {(Object.keys(show.occupiedSeats || {}).length) * show.showPrice}
+                </td>
 
               </tr>
             ))}
