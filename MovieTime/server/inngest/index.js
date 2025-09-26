7cfd6e9304150ce16329502e407a97c25f423d2a
vqt-zz <tuanvq@larion.com>
2025-09-26 09:55:51 +0700

    fix: update movie model reference to match casing in booking confirmation email function

--- MovieTime/server/inngest/index.js
+++ MovieTime/server/inngest/index.js
@@ -89,7 +89,7 @@ const sendBookingConfirmationEmail = inngest.createFunction(
     const {bookingId} = event.data;
     const booking = await Booking.findById(bookingId).populate({
       path: 'show',
-      populate: {path: "movie", model: "movie"}
+      populate: {path: "movie", model: "Movie"}
     }).populate('user');
 
     await sendEmail({
