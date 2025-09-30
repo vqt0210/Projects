7c550dfc274c46d71fbfc5a07ba782a374656a7d
vqt-zz <tuanvq@larion.com>
2025-09-30 13:47:33 +0700

    Refactor code structure for improved readability and maintainability

--- MovieTime/client/src/components/HeroSection.jsx
+++ MovieTime/client/src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/assets/demonslayer.jpg")] bg-cover bg-center h-screen'>
       <img src={assets.demonIcon} alt="" className=' flex max-h-40 lg:h-50 mt-20' />
 
-      <h1 style={{ fontFamily: 'BloodCrow' }} className='text-5xl md:text-[70px] md:leading-18 font bold max-w-xl '>Demon Slayer Infinity Castle</h1>
+      <h1 style={{ fontFamily: 'BloodCrow' }} className='text-5xl md:text-[70px] md:leading-18 max-w-xl '>Demon Slayer Infinity Castle</h1>
 
       <div className='flex items-center gap-4 text-gray-300'>
         <span>Action | Adventure | Animation</span>
