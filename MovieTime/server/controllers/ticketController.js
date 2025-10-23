3288b93f1a9f194cae8a061cc506c6cf9ec3b369
vqt-zz <tuanvq@larion.com>
2025-10-23 18:00:17 +0700

    fix(ticket): ensure consistent bookedSeats field and prevent undefined crash

--- MovieTime/server/controllers/ticketController.js
+++ MovieTime/server/controllers/ticketController.js
@@ -25,7 +25,7 @@ export const getTicketById = async (req, res) => {
         movieTitle: movie.title,
         moviePoster: movie.poster_path,
         showDateTime: booking.show.showDateTime,
-        seats: booking.bookedSeats,
+        bookedSeats: booking.bookedSeats,
         amount: booking.amount,
         status: booking.status,
         qrCode: booking.qrCode,
